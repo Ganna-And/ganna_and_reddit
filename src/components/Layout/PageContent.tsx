12ac46f7534f93aeb68a4210b54f9bc38d192012
= <ganna.andrushko@gmail.com>
2023-05-03 10:23:58 +0800

    extended inteface reactPropsWithChildren

--- src/components/Layout/PageContent.tsx
+++ src/components/Layout/PageContent.tsx
@@ -1,9 +1,7 @@
 import { Flex } from '@chakra-ui/react';
 import React from 'react';
  
-type PageContentProps = {
-    
-};
+interface PageContentProps extends React.PropsWithChildren<{}> {}
 
 const PageContent:React.FC<PageContentProps> = ({children}) => {
     
